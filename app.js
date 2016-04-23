33808393f48b66b741e8c068a7cd0bcaae8a1019
Alex Ellis <alexellis2@gmail.com>
2016-04-23 23:37:08 +0100

    Redirection update

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.disable('x-powered-by');
 app.use("/", express.static('public'));
 app.use("/dist/", express.static('bower_components'));
 
+app.use("/goto/:id", function(req, res) {
+  if(req.params.id == "github") {
+    return res.redirect(302, "https://github.com/alexellis/");
+  } else if(req.params.id == "blog") {
+    return res.redirect(302, "http://blog.alexellis.io/");
+  }
+  res.end(404);
+});
+
 app.listen(3000, function() {
     console.log("alexellis.io started");
 });
