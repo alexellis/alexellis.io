ea20f1a3b3e32b9d3a459e8402c956563d8a2bfd
Alex Ellis <alexellis2@gmail.com>
2016-12-12 16:37:52 +0000

    Update up_test.js

--- up_test.js
+++ up_test.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 describe("Availability test", function() {
   it("is up", function(done) {
     request.get("http://localhost:3000", function(err, res, body) {
-      assert.ok(res.status=200, "Did not get OK status");
+      assert.ok(res.status==200, "Did not get OK status");
       done();
     });
   });
