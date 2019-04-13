7e848739d8736e0cde405430f259e078cb73083e
Alex Ellis <alexellis2@gmail.com>
2019-04-13 08:58:13 +0100

    Update social media icons
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- app.js
+++ app.js
@@ -10,29 +10,6 @@ app.disable('x-powered-by');
 app.use("/", express.static('public'));
 app.use("/dist/", express.static('bower_components'));
 
-app.use("/inlets", function(req, res) {
-  return res.redirect(302, "https://raw.githubusercontent.com/alexellis/inlets/master/get.sh");
-});
-
-app.use("/goto/:id", function(req, res) {
-  if(req.params && req.params.id) {
-
-    switch(req.params.id) {
-      case "github":
-        return res.redirect(302, "https://github.com/alexellis/");
-      case "blog":
-        return res.redirect(302, "http://blog.alexellis.io/");
-      case "linkedin":
-        return res.redirect(302, "https://www.linkedin.com/in/alexellisuk/");
-      case "openfaas":
-        return res.redirect(302, "https://www.openfaas.com/");
-      case "twitter":
-        return res.redirect(302, "https://twitter.com/alexellisuk");
-    }
-  }
-  res.end(404);
-});
-
 app.listen(port, function() {
     console.log(`alexellis.io started on port: ${port}.`);
 });
