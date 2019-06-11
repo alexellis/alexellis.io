222ed79624061da58f451bbb33e8f9cb9e814a7c
Alex Ellis <alexellis2@gmail.com>
2019-06-11 16:41:50 +0100

    Bump Docker image
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- create.sh
+++ create.sh
@@ -2,7 +2,9 @@
 
 docker rm -f alexellisio
 
+TAG="alexellis2/alexellisio:2019-06-11"
+
 docker run --name alexellisio \
  -p 3000:3000 \
  --restart=always \
- -d alexellis2/alexellisio:2019-06-07
+ -d ${TAG}
