704bb624d1002d7ba7b3ad225ab81707f2226a20
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2025-08-05 12:02:14 +0100

    Update to paths
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "Alex Ellis - Cloud Native Consultant",
-  description: "Personal website of Alex Ellis, CNCF Ambassador and founder of OpenFaaS",
+  description: "Connect with Alex Ellis, founder of OpenFaaS, Actuated, and Inlets",
 }
 
 export default function RootLayout({
--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/alexellis.io',
+  assetPrefix: '/alexellis.io/',
   images: {
     unoptimized: true,
     remotePatterns: [
